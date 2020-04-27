68a208223774e395458559e99b6ff8b4de9afeab
FIRST_tristan LAST_richard <tristan3755@gmail.com>
2020-04-27 16:20:45 +0200

    pacman

--- javascript.js
+++ javascript.js
@@ -16,12 +16,26 @@ let maGrille = [
     [0,2,2,2,2,2,2,2,2,0,2,2,2,2,2,2,2,2,0],
     [0,2,0,0,2,0,0,0,2,0,2,0,0,0,2,0,0,2,0],
     [0,2,2,0,2,2,2,2,2,2,2,2,2,2,2,0,0,2,0],
-    [0,0,2,0,2,0,2,0,0,0,0,0,2,0,2,0,2,0,0],
+    [0,0,2,0,2,0,2,0,0,0,0,0,2,0,2,0,0,2,0],
     [0,2,2,2,2,0,2,2,2,0,2,2,2,0,2,2,2,2,0],
     [0,2,0,0,0,0,0,0,2,0,2,0,0,0,0,0,0,2,0],
     [0,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,0],
     [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
 ]
+
+let pacman = {
+  
+   y:2, 
+   x:5,
+  
+   direction:1
+
+ }
+
+
+function affichageGrille(){
+
+  document.getElementById('grillePacman').innerHTML= " "
 
 
 
@@ -32,6 +46,7 @@ for(let i in maGrille){
     
     
     var monElem=document.createElement('div')
+  
 
     if(maGrille[i][j]==0){
     
@@ -51,10 +66,44 @@ for(let i in maGrille){
     monElem.classList.add("bonbon")
     
     }
-    document.getElementById('grillePacman').appendChild(monElem)
+
+
+        monElem.style.gridColumn=parseInt(j)+1;
+        monElem.style.gridRow=parseInt(i)+1;
+        document.getElementById('grillePacman').appendChild(monElem)
+
   }
 
  
 
 }
+
+}
+function affichePacman(){
+
+
+  let blocPacman = document.createElement('div');
+        blocPacman.id='pacman';
+        blocPacman.style.gridColumn=pacman.x+1;
+        blocPacman.style.gridRow=pacman.y+1;
+
+
+document.getElementById('grillePacman').appendChild(blocPacman)
+
+
+
+}
+
+function refresh()
+{
   
+  
+  affichageGrille()
+  affichePacman()
+  setTimeout(refresh, 1000)
+
+}
+
+
+refresh()
+
